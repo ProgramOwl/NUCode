a4d2e1880bebde2cc89ce97a7a0f4e35a54a5221
DShoupe <dylanshoupe@yahoo.com>
2017-11-07 19:00:03 -0700

    Remade project with correct add ons

---
+++ NUCode/NUCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NUCode.Startup))]
+namespace NUCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
